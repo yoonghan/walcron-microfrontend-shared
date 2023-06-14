6f9bd5ede4a2301ccebd69746ace715f7febb99d
han <walcoorperation@gmail.com>
2023-06-15 07:04:11 +0800

    Readd dist folder

---
+++ dist/components/Logo/index.d.ts
@@ -0,0 +1,11 @@
+interface Props {
+    /**
+     * Color of logo
+     */
+    color?: "green" | "blue" | "yellow";
+}
+/**
+ * Walcron logo and symbol
+ */
+declare const Logo: ({ color }: Props) => import("react/jsx-runtime").JSX.Element;
+export default Logo;
