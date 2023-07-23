dfaf4961555d4ebc794eb5725b6be9f7eeb535a5
han <walcoorperation@gmail.com>
2023-07-24 03:38:34 +0800

    Export component for distribution.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { default as Logo } from "./components/Logo";
 export { default as Copyright } from "./components/Copyright";
+export { default as Footer } from "./components/Footer";
+export * from "./components/helpers";
