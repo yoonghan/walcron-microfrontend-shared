11afc68712e8f521e5391c7a02c136f51b022ae8
han <walcoorperation@gmail.com>
2024-05-04 15:10:28 +0800

    Update footer.

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -6,14 +6,14 @@ describe("Footer", () => {
   const currentYear = new Date().getFullYear();
 
   it("should display year with default value", () => {
-    render(<Footer lastUpdatedYear={2023} />);
+    render(<Footer lastUpdatedYear={2024} />);
     const footerMessage = `Walcron 2014 - ${currentYear} ©`;
     expect(screen.getByText(footerMessage)).toBeInTheDocument();
   });
 
   it("should override company name and year started", () => {
     render(
-      <Footer companyName="Abc" establishedYear={2020} lastUpdatedYear={2023} />
+      <Footer companyName="Abc" establishedYear={2020} lastUpdatedYear={2024} />
     );
     const footerMessage = `Abc 2020 - ${currentYear} ©`;
     expect(screen.getByText(footerMessage)).toBeInTheDocument();
