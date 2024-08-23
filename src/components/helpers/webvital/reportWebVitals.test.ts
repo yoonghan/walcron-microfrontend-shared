eca53e0928b2ed5d279da09f092520c4b168bbd7
han <walcoorperation@gmail.com>
2024-08-24 07:19:46 +0800

    Update CLS report for webvitals

--- src/components/helpers/webvital/reportWebVitals.test.ts
+++ src/components/helpers/webvital/reportWebVitals.test.ts
@@ -1,9 +1,26 @@
-import { describe, it } from "vitest";
-import reportWebVitals from "./reportWebVitals";
+import { describe, it, vi, expect } from "vitest";
+import reportWebVitals, { updateCLS } from "./reportWebVitals";
 import { consoleReportHandler } from "..";
+import { Metric } from "web-vitals";
 
 describe("reportWebVitals", () => {
   it("can be reported from consoleReportHandler", () => {
     reportWebVitals(consoleReportHandler);
+  });
+
+  it("update CLS 1000 times as decimal is given", () => {
+    const call = vi.fn();
+    updateCLS(call)({
+      delta: 0.01,
+      id: "delta",
+      name: "CLS",
+      value: 1,
+    } as Metric);
+    expect(call).toHaveBeenCalledWith({
+      delta: 10,
+      id: "delta",
+      name: "CLS",
+      value: 1,
+    });
   });
 });
