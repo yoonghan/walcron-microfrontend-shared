434986bcbb29344c76918b337cc3644e0cec9db3
han <walcoorperation@gmail.com>
2024-10-11 16:15:47 +0800

    Allow card to leave small gaps and set min height.

--- pages/scrollable-card/+Page.tsx
+++ pages/scrollable-card/+Page.tsx
@@ -1,3 +1,4 @@
+import "./code.css";
 import ScrollableCard from "../../src/components/ScrollableCard";
 import React from "react";
 
@@ -5,17 +6,33 @@ export { Page };
 
 const cards = [
   {
-    content: <div style={{ backgroundColor: "white" }}>My First card</div>,
+    content: (
+      <div style={{ backgroundColor: "orange", height: "100%" }}>
+        My First card
+      </div>
+    ),
   },
 
   {
-    content: <div style={{ backgroundColor: "blue" }}>My Second card</div>,
+    content: (
+      <div style={{ backgroundColor: "blue", height: "100%" }}>
+        My Second card
+      </div>
+    ),
   },
   {
-    content: <div style={{ backgroundColor: "green" }}>My Third card</div>,
+    content: (
+      <div style={{ backgroundColor: "green", height: "100%" }}>
+        My Third card
+      </div>
+    ),
   },
   {
-    content: <div style={{ backgroundColor: "red" }}>My Fourth card</div>,
+    content: (
+      <div style={{ backgroundColor: "red", height: "100%" }}>
+        My Fourth card
+      </div>
+    ),
   },
 ];
 
@@ -23,8 +40,11 @@ function Page() {
   return (
     <>
       <h1>Scrollable Card</h1>
-
-      <ScrollableCard model={cards} />
+      <div>
+        <strong>NOTE:</strong> If you need to override per-card, use --index
+      </div>
+      <ScrollableCard model={cards} className="container" />
+      <div style={{ height: "100vh" }}>Footer</div>
     </>
   );
 }
--- src/components/ScrollableCard/index.tsx
+++ src/components/ScrollableCard/index.tsx
@@ -5,9 +5,15 @@ interface Model {
   content: ReactNode;
 }
 
-function ScrollableCard({ model }: { model: Model[] }) {
+function ScrollableCard({
+  model,
+  className,
+}: {
+  model: Model[];
+  className: string;
+}) {
   return (
-    <ul className={styles.container}>
+    <ul className={`${styles.container} ${className}`}>
       {model.map(({ content }, idx) => (
         <li key={`sc-${idx}`}>{content}</li>
       ))}
