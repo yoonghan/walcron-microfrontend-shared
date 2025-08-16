52f15a39a7ce1e96c735fb1a207b1bc80116f19c
han <mailyoonghan@gmail.com>
2025-08-17 06:57:06 +0800

    Fix menu test case.

--- src/components/Menu/Menu.jest.test.tsx
+++ src/components/Menu/Menu.jest.test.tsx
@@ -271,45 +271,46 @@ describe("Menu", () => {
     })
   })
 
-  const renderMenu = render(
-    <Menu
-      menuLink={MenuLink}
-      homeLink={HomeLink}
-      homeLogoLink={HomeLogoLink}
-      model={[
-        {
-          label: "Top Menu",
-          url: "/top-menu",
-          items: [
-            {
-              label: "About Us",
-            },
-            {
-              label: "What about us",
-            },
-          ],
-        },
-        {
-          label: "Top Menu 2",
-          url: "/top-menu-2",
-          items: [
-            {
-              label: "About Us 2",
-            },
-          ],
-        },
-        {
-          label: "News",
-          url: "/sample-us",
-        },
-      ]}
-      mobileStyle={{ display: "block" }}
-      desktopStyle={{ display: "none" }}
-    />,
-  )
+  const renderMenuAccessibility = (isDesktop = false) =>
+    render(
+      <Menu
+        menuLink={MenuLink}
+        homeLink={HomeLink}
+        homeLogoLink={HomeLogoLink}
+        model={[
+          {
+            label: "Top Menu",
+            url: "/top-menu",
+            items: [
+              {
+                label: "About Us",
+              },
+              {
+                label: "What about us",
+              },
+            ],
+          },
+          {
+            label: "Top Menu 2",
+            url: "/top-menu-2",
+            items: [
+              {
+                label: "About Us 2",
+              },
+            ],
+          },
+          {
+            label: "News",
+            url: "/sample-us",
+          },
+        ]}
+        mobileStyle={{ display: isDesktop ? "none" : "block" }}
+        desktopStyle={{ display: isDesktop ? "block" : "none" }}
+      />,
+    )
 
   describe("accessibility for desktop", () => {
-    const renderDesktopWithAccessibility = renderMenu
+    const renderDesktopWithAccessibility = () => renderMenuAccessibility(true)
 
     it("should show aria-expanded=true when top menu's subbutton is clicked and has submenus on click again will close", async () => {
       renderDesktopWithAccessibility()
@@ -393,7 +394,7 @@ describe("Menu", () => {
   })
 
   describe("accessibility for mobile", () => {
-    const renderMobileWithAccessibility = renderMenu
+    const renderMobileWithAccessibility = () => renderMenuAccessibility(false)
 
     it("should show aria-expanded=true when top menu's subbutton is clicked and has submenus on click again will close", async () => {
       renderMobileWithAccessibility()
