320fe656bb93ea9d5c1cea1cf10e9922cac93608
han <mailyoonghan@gmail.com>
2025-08-17 06:23:42 +0800

    Unique page.

--- pages/scrollable-card/+Page.tsx
+++ pages/scrollable-card/+Page.tsx
@@ -1,38 +1,32 @@
 import "./code.css"
 import ScrollableCard from "../../src/components/ScrollableCard"
-import React from "react"
-
 export { Page }
 
-const cards = [
-  {
-    content: (
-      <div style={{ backgroundColor: "orange", height: "100%" }}>
-        My First card
-      </div>
-    ),
-  },
-
-  {
-    content: (
-      <div style={{ backgroundColor: "blue", height: "100%" }}>
-        My Second card
-      </div>
-    ),
-  },
+const card = [
   {
     content: (
       <div style={{ backgroundColor: "green", height: "100%" }}>
-        My Third card
+        Reversed First card
       </div>
     ),
+    id: "1st",
+  },
+
+  {
+    content: (
+      <div style={{ backgroundColor: "yellow", height: "100%" }}>
+        Reversed Second card
+      </div>
+    ),
+    id: "2nd",
   },
   {
     content: (
-      <div style={{ backgroundColor: "red", height: "100%" }}>
-        My Fourth card
+      <div style={{ backgroundColor: "white", height: "100%" }}>
+        Reversed Third card
       </div>
     ),
+    id: "3rd",
   },
 ]
 
@@ -43,7 +37,7 @@ function Page() {
       <div>
         <strong>NOTE:</strong> If you need to override per-card, use --index
       </div>
-      <ScrollableCard model={cards} className="container" />
+      <ScrollableCard model={card} className="container" />
       <div style={{ height: "100vh" }}>Footer</div>
     </>
   )
